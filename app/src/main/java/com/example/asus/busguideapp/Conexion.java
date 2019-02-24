ff89b2e2b19bfe7822044e8dca9a35471cf28a40
alu0100977103 <alu0100977103@ull.edu.es>
2019-02-24 14:34:51 +0100

    Detección de dispositivos correcta

--- app/src/main/java/com/example/asus/busguideapp/Conexion.java
+++ app/src/main/java/com/example/asus/busguideapp/Conexion.java
@@ -1,106 +1,48 @@
 package com.example.asus.busguideapp;
 
-import android.app.Dialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.drm.DrmStore;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.RecyclerView.Adapter;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 
-
 public class Conexion extends AppCompatActivity{
-    class Dispo{
-        String nombre;
-        String direccion;
-
-        Dispo(String nombre, String direccion){
-            this.nombre=nombre;
-            this.direccion=direccion;
-        }
-
-    }
-
-    private List<Dispo> dispositives;
-    private void initializeData(){
-        dispositives=new ArrayList<>();
-        dispositives.add(new Dispo("Dispositivo 1","33333"));
-    }
-    private static final String TAG="Conexion";
-    private static final int REQUEST_ENABLE_BT = 1;
-    private BluetoothAdapter mBluetoothAdapter;
+    private String TAG = "Conexion";
+    Button buscar;
+    BluetoothAdapter mBluetoothAdapter;
+    ListView listview;
+    private ArrayList<String> mDeviceList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.connexion);
+
+        buscar=findViewById(R.id.buscar);
+        listview = findViewById(R.id.listview);
+
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        RecyclerView recyclerView=findViewById(R.id.dispositivos);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        RVAdapter adapter=new RVAdapter(dispositives);
-
-        recyclerView.setAdapter(adapter);
-
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
-        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-        filter.addAction(BluetoothDevice.ACTION_FOUND);
-
-        registerReceiver(mReceiver, filter);
     }
 
     public void InicioBusqueda(View view){
-        if(mBluetoothAdapter.isDiscovering()){
-            Log.d(TAG,"InicioBusqueda: Ya se esta buscando dispositivo");
-        }else if(mBluetoothAdapter.startDiscovery()){
-            Log.d(TAG,"InicioBusqueda: Buscando dispositivos");
-        }else{
-            Log.d(TAG,"InicioBusqueda: Error al buscar el dispositivo");
+        Set<BluetoothDevice> devices =mBluetoothAdapter.getBondedDevices();
+        for(BluetoothDevice device: devices){
+            mDeviceList.add(device.getAddress());
+            listview.setAdapter(new ArrayAdapter<>(getApplication(),android.R.layout.simple_list_item_1,mDeviceList ));
+            Log.wtf(TAG, String.valueOf(device.getAddress()));
         }
     }
-
-    protected void onDestroy(){
-        unregisterReceiver(mReceiver);
-        super.onDestroy();
-    }
-
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-
-            assert action != null;
-            switch (action){
-                case BluetoothAdapter.ACTION_DISCOVERY_STARTED:
-                    Log.d(TAG,"onReceive: Busqueda de dispositivos iniciada");
-                    break;
-                case BluetoothAdapter.ACTION_DISCOVERY_FINISHED:
-                    Log.d(TAG,"onReceive: Busqueda de dispositivos finalizada");
-                    break;
-                case BluetoothDevice.ACTION_FOUND:
-                    BluetoothDevice device=intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                    dispositives.add(new Dispo(device.getName(),device.getAddress()));
-                    break;
-            }
-        }
-    };
 
 }
